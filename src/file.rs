3a9f40825924c7247dbf063568a9727e4db65bd1
DelSkayn <yoko.minsky@gmail.com>
2015-10-06 18:39:40 +0200

    Made almost no progress on resources

---
+++ src/file.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::fs::Path;
+use std::io::Error;
+
+#[derive(Debug)]
+struct FileFolder{
+    pub folder: Vec<FileFolder>,
+    pub files: Vec<String>,
+    pub path: Path,
+}
+
+struct FileSystem{
+    pub root: FileFolder,
+}
+
+enum FileSystemError{
+    RootDirNotAFolder,
+    OsIOError,
+}
+
+impl Into<Error> for FileSystemError{
+    fn into(err: Error) -> FileSystemError{
+        FileSystemError::OsIOError
+    }
+}
+
+impl FileSystem{
+    fn new(path: Path) -> Result<Self,FileSystemError>{
+        if !try!(path.metadata()).is_dir(){
+            return Err(FileSystemError::RootDirNotAFolder);
+        }else{
+        }
+    }
+}
--- src/resource.rs
+++ src/resource.rs
@@ -4,6 +4,13 @@ trait Resource{
     fn get_size() -> usize;
 }
 
+trait ResourceLoader{
+    type Output: Resource;
+    type LoadData;
+
+    fn load(LoadData) -> Self::Output;
+}
+
 struct ResourceHandeler{
     resource_map: HashMap<String,Rc<Resource>>,
 }
