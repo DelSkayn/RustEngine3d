ad212e8ed3da28ac5b9763dc821ee7fda49203cb
DelSkayn <yoko.minsky@gmail.com>
2016-02-15 16:24:33 +0100

    Updated for new gluim

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -82,6 +82,7 @@ impl From<index::BufferCreationError> for BufferError{
 
 #[derive(Debug)]
 pub enum ShaderCreationError{
+    BinaryHeaderError,
     CompilationError(String),
     LinkingError(String),
     ShaderTypeNotSupported,
@@ -93,6 +94,7 @@ pub enum ShaderCreationError{
 impl From<program::ProgramCreationError> for ShaderCreationError{
     fn from(err: program::ProgramCreationError) -> Self{
         match err {
+            program::ProgramCreationError::BinaryHeaderError => ShaderCreationError::BinaryHeaderError,
             program::ProgramCreationError::CompilationError(x) => ShaderCreationError::CompilationError(x),
             program::ProgramCreationError::LinkingError(x) => ShaderCreationError::LinkingError(x),
             program::ProgramCreationError::ShaderTypeNotSupported => ShaderCreationError::ShaderTypeNotSupported,
