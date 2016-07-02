45e4e3da64daf0a3c25ed88961d3ccd3d8aaf9c1
DelSkayn <yoko.minsky@gmail.com>
2016-07-03 00:50:33 +0200

    System is now in a state where it is compiling! Finaly!

--- tungsten/src/logic/system.rs
+++ tungsten/src/logic/system.rs
@@ -6,178 +6,111 @@ use super::component::{
     ComponentStorageBorrowWriteGuard,
 };
 
-use task;
+use task::{self,DynTaskImpl,ArcLatch,Latch};
 
-use std::sync::atomic::{AtomicUsize, Ordering};
-
-use std::cell::{Cell,UnsafeCell};
-use std::sync::Arc;
+use std::mem;
+use std::cell::UnsafeCell;
+use std::marker::PhantomData;
 
 pub type ArgReadGaurd<'a,T> = ComponentStorageBorrowReadGuard<'a,T>;
 pub type ArgWriteGaurd<'a,T> = ComponentStorageBorrowWriteGuard<'a,T>;
 
 /// Struct for handeling system data needs.
+/// TODO: Test wether this allows for unsafe code.
 #[derive(Clone)]
-pub struct Args<'a>{
+pub struct Args{
+    components: *const Components,
+}
+
+unsafe impl Send for Args{}
+
+impl Args{
+    pub fn borrow<'a,T: ComponentStorage>(&'a self) -> Option<ArgReadGaurd<'a,T>>{
+        unsafe{
+            (*self.components).borrow::<T>()
+        }
+    }
+
+    pub fn borrow_mut<'a,T: ComponentStorage>(&'a self) -> Option<ArgWriteGaurd<'a,T>>{
+        unsafe{
+            (*self.components).borrow_mut::<T>()
+        }
+    }
+}
+
+pub struct Schedular<'a>{
     components: &'a Components,
 }
 
-impl<'a> Args<'a>{
-    pub fn borrow<T: ComponentStorage>(&'a self) -> Option<ArgReadGaurd<'a,T>>{
-        self.components.borrow::<T>()
+
+pub struct SchedularGaurd<'a>
+{
+    func: Option<DynTaskImpl<ArcLatch,()>>,
+    latch: ArcLatch,
+    _marker: PhantomData<&'a ()>
+}
+
+impl<'a> SchedularGaurd<'a>
+{
+    pub fn wait(self){
+        mem::drop(self);
     }
 
-    pub fn borrow_mut<T: ComponentStorage>(&self) -> Option<ArgWriteGaurd<'a,T>>{
-        self.components.borrow_mut::<T>()
-    }
-}
-
-pub trait System: Sync + 'static {
-    /// Executes an system returns wether the system was properly
-    /// executed or needs to be executed again.
-    /// If system was not properly executed the state of the world was not changed,
-    fn execute<'a>(&'a mut self,arg: Args<'a>) -> bool;
-}
-
-pub struct SinkSystemInner{
-    system: UnsafeCell<Box<System>>,
-    count: Cell<usize>,
-    dep_count: AtomicUsize,
-}
-
-unsafe impl Sync for SinkSystemInner{}
-unsafe impl Send for SinkSystemInner{}
-
-pub struct SinkSystem(Arc<SinkSystemInner>);
-
-impl SinkSystem{
-    fn new<S: System>(s: S) -> Self{
-        SinkSystem(Arc::new(
-                SinkSystemInner{
-                    system: UnsafeCell::new(Box::new(s)),
-                    count: Cell::new(1),
-                    dep_count: AtomicUsize::new(1),
-                }
-                ))
+    pub fn is_done(&self) -> bool{
+        self.latch.done()
     }
 }
 
-impl Clone for SinkSystem{
-    fn clone(&self) -> Self{
-        let cur = self.0.count.get();
-        self.0.count.set(cur + 1);
-        self.0.dep_count.store(self.0.count.get(),Ordering::Release);
-        SinkSystem(self.0.clone())
-    }
-}
-
-impl System for SinkSystem{
-    fn execute<'a>(&'a mut self,arg: Args<'a>) -> bool{
-        if self.0.dep_count.load(Ordering::Acquire) == 0{
-            let res = unsafe{ (*self.0.system.get()).execute(arg) };
-            if res {
-                self.0.dep_count.store(self.0.count.get(),Ordering::Release);
-            }
-            res
-        }else if self.0.dep_count.fetch_sub(1,Ordering::AcqRel) == 1{
-            // each parent has completed and executed there childs.
-            let res = unsafe{ (*self.0.system.get()).execute(arg) };
-            if res {
-                self.0.dep_count.store(self.0.count.get(),Ordering::Release);
-            }
-            res
-        }else{
-            true
+impl<'a> Drop for SchedularGaurd<'a>
+{
+    fn drop(&mut self) {
+        while !self.latch.done(){
+            task::steal();
         }
-    }
-}
-
-pub trait IntoSystemData{
-    fn into_system_data(self) -> SystemData;
-}
-
-impl<S: System> IntoSystemData for S{
-    fn into_system_data(self) -> SystemData{
-        SystemData{
-            system: UnsafeCell::new(Box::new(self)),
-            childs: None,
-        }
-    }
-}
-
-impl IntoSystemData for SystemBuilder{
-    fn into_system_data(self) -> SystemData{
-        self.0
-    }
-}
-
-pub struct SystemData{
-    system: UnsafeCell<Box<System>>,
-    childs: Option<Vec<SystemData>>,
-}
-
-unsafe impl Sync for SystemData{}
-
-pub struct SystemBuilder(SystemData);
-
-impl SystemBuilder{
-    pub fn new<S: System>(system: S) -> Self{
-        SystemBuilder(
-            SystemData{
-                system: UnsafeCell::new(Box::new(system)),
-                childs: None,
-            })
-    }
-
-    pub fn child<S: IntoSystemData>(&mut self,s: S){
-        if let None = self.0.childs{
-            self.0.childs = Some(Vec::new());
-        }
-        self.0.childs.as_mut().unwrap().push(s.into_system_data());
-    }
-}
-
-pub struct Systems{
-    systems: Vec<SystemData>,
-}
-
-impl Systems{
-    pub fn new() -> Self{
-        Systems{
-            systems: Vec::new(),
-        }
-    }
-
-    pub fn add<S: IntoSystemData>(&mut self,system: S){
-        self.systems.push(system.into_system_data());
-    }
-
-    pub fn execute(&mut self,components: &Components){
-        schedule(&self.systems,Args{
-            components: components
-        });
-    }
-}
-
-fn schedule<'a>(systems: &[SystemData],arg: Args<'a>){
-    if systems.len() == 1{
+        // Propagate posible panic.
         unsafe{
-            while !(*systems[0].system.get()).execute(arg.clone()){}
+            self.func.take().unwrap().into_result();
         }
-        if let Some(x) = systems[0].childs.as_ref(){
-            schedule(x,arg);
-        }
-
-    }else{
-        let mid = systems.len() / 2;
-        let (fir,sec) = systems.split_at(mid);
-        let arg_c = arg.clone();
-        task::join(||{
-            schedule(fir,arg_c);
-        }
-        ,||{
-            schedule(sec,arg);
-        });
-
     }
 }
+
+impl<'a> Schedular<'a>{
+    pub fn execute<F>(&mut self,func: F) -> SchedularGaurd<'a>
+        where F: FnMut(Args) -> bool + Send + 'a,
+    {
+        let mut res = SchedularGaurd{
+            func: None,
+            latch: ArcLatch::new(),
+            _marker: PhantomData,
+        };
+        let arg = Args{ components: self.components};
+        res.func = unsafe{
+            let temp = Some(DynTaskImpl::new(res.latch.clone(),move || {
+                    let mut func = func;
+                    while !func(arg.clone()){
+                        task::steal();
+                    }
+            }));
+            task::push_unchecked(temp.as_ref().unwrap());
+            temp
+        };
+        res
+    }
+
+    fn w1r0run<A,F>(&mut self,func: F) 
+        where F: FnOnce(&mut A) + Send + 'a,
+              A: ComponentStorage 
+    {
+        let func_opt = UnsafeCell::new(Some(func));
+        self.execute(move |args|{
+            let mut a = if let Some(x) = args.borrow_mut::<A>(){ x }else{return true;};
+            // TODO: Find a better way.
+            unsafe{
+                (*func_opt.get()).take().unwrap()(&mut *a);
+            }
+            true
+        });
+    }
+
+}
+
